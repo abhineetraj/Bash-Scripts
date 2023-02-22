42bed79f5edf33f7e81bfb3fb8f824058be07fef
Abhineet Raj <abhineetraj@hotmail.com>
2023-02-22 13:29:59 -0800

    Create count-ip-nginx.sh
    Count the number of IP's in the nginx logs

---
+++ count-ip-nginx.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+:> /tmp/updated_logs
+:> /tmp/updated_ips
+zero=0
+
+#define the global function to check the input
+check_number()
+{
+re='^[0-9]+$'
+if ! [[ $hrs =~ $re ]] ; then
+   echo "Duration is not an integer" >&2; exit 1
+elif [[ $hrs -gt 4 ]];
+then
+   echo "Duration is more than 4 hours, please enter less than 5 hours" && exit 1
+fi
+}
+
+
+printf "Enter the duration to find the count of unique IP's from nginx logs? (1/2/3/4)\n"
+read hrs
+check_number
+
+#grep the nginx logs
+for (( i=$zero; i<=$hrs; i++ ))
+do
+    grep `date -d now-${i}hours  +%d/%b/%Y:%H` /var/log/nginx/access.log >> /tmp/updated_logs
+done
+
+#Calculate the ip's
+awk '{print $1}' /tmp/updated_logs > /tmp/updated_ips
+printf "Logs are stored @ /tmp/updated_logs\n" && sleep 2
+printf "Printing the count of IP's for the last $hrs hours \n"
+source /var/local/adm/progress.sh && progressbar 5
+sort /tmp/updated_ips | uniq -c | sort -n -r -k1,1 | head -40
